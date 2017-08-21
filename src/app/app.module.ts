eece792325b9775958bb7e8ffbfc0b661ee0506d
Brook Hutchinson <brookhutchinson@outlook.com>
2017-08-21 17:23:13 -0500

    Create NavbarComponent, Register NavbarComponent with AppModule, Import NavbarComponent into AppModule, Add NavbarComponent to @NgModule Decorator "declarations" property array, Define NavbarComponent TypeScript, Define property named "brand" of type string, Set "brand" property value inside ngOnInit() function, Define NavbarComponent HTML Template, Use data binding expression and string interpolation to render "brand" property value inside NavbarComponent HTML Template, Define CSS to move menu items to the right 10px of "brand" property value inside NavbarComponent HTML Template, Add <navbar> directive inside AppComponent HTML Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 // modules
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule }    from '@angular/http';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }        from '@angular/core';
+import { BrowserModule }   from '@angular/platform-browser';
+import { HttpModule }      from '@angular/http';
+import { FormsModule }     from '@angular/forms';
 
 // components
-import { AppComponent }  from './app.component';
+import { AppComponent }    from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   // modules
   imports: [ BrowserModule, HttpModule, FormsModule ],
   // components
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent ],
   // services
   providers: [],
   // bootstrap
