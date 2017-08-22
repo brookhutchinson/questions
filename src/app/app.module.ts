12a88a70be6e18d7c5e7cb3de32c69e18ec57607
Brook Hutchinson <brookhutchinson@outlook.com>
2017-08-22 09:20:38 -0500

    Create QuestionListComponent, Import QuestionListComponent into AppModule, Add QuestionListComponent to @NgModule Decorator "declarations" property array, Define QuestionListComponent TypeScript, Define public property named "questions" that is an array of objects, Set the "questions" property to an array of objects inside the ngOnInit() function, Each object represents a question/answer pair, Each object will contain two properties "text" and "answer", Define QuestionListComponent HTML Template, Define a <div class="container">, Define <div class="question"> for each question object inside "questions" property, Use the *ngFor directive to loop through each question inside "questions" property, Define a <div class="card"> inside <div class="question">, Define a <div class="card-header"> inside <div class="card">, Define a <div class="card-body"> inside <div class="card">, Use a data binding expression and string interpolation to render the question.text property inside the <div class="card-header">, Use a data binding expression and string interpolation to render the question.answer property inside the <div class="card-body">, Add <question-list> directive inside AppComponent HTML Template, Add a <br> between the <navbar> directive and the <question-list> directive inside AppComponent HTML Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 // modules
-import { NgModule }        from '@angular/core';
-import { BrowserModule }   from '@angular/platform-browser';
-import { HttpModule }      from '@angular/http';
-import { FormsModule }     from '@angular/forms';
+import { NgModule }              from '@angular/core';
+import { BrowserModule }         from '@angular/platform-browser';
+import { HttpModule }            from '@angular/http';
+import { FormsModule }           from '@angular/forms';
 
 // components
-import { AppComponent }    from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppComponent }          from './app.component';
+import { NavbarComponent }       from './components/navbar/navbar.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
 
 @NgModule({
   // modules
   imports: [ BrowserModule, HttpModule, FormsModule ],
   // components
-  declarations: [ AppComponent, NavbarComponent ],
+  declarations: [ AppComponent, NavbarComponent, QuestionListComponent ],
   // services
   providers: [],
   // bootstrap
---
+++ src/app/components/question-list/question-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'question-list',
+  templateUrl: './question-list.component.html',
+  styleUrls: ['./question-list.component.css']
+})
+export class QuestionListComponent implements OnInit {
+  // define properties
+  public questions: Object[];
+
+  constructor() {}
+
+  ngOnInit() {
+    // set properties
+    this.questions = [
+      { text: 'Who is your favorite music artist?', answer: 'Bob Schneider' },
+      { text: 'What is the name of your favorite song?', answer: 'Hideaway' },
+      { text: 'What is the name of your second favorite song?', answer: '40 Dogs' }
+    ];
+  }
+}
