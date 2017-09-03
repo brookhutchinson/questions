d8aea45285e8d37dc5e6d811068b66d44374ba3c
Brook Hutchinson <brookhutchinson@outlook.com>
2017-09-03 17:47:42 -0500

    Use DataService to add question to Local Storage, Redefine DataService addQuestion() function

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,6 +11,7 @@ export class DataService {
 
   constructor() {}
 
+  // get questions from Local Storage
   getQuestions() {
     // check if value is stored inside Local Storage and then set questions property value
     if (localStorage.getItem('questions') === null) {
@@ -29,17 +30,46 @@ export class DataService {
     return this.questions;
   }
 
+  // add question to Local Storage
   addQuestion(question: Question) {
-    // add new question object to "questions"
+    // add new question object to questions property array of objects
     this.questions.unshift(question);
+
+    // initialize local variable
+    let questions;
+
+    // check if value is not stored inside Local Storage
+    if (localStorage.getItem('questions') === null) {
+      // questions property is not stored inside Local Storage
+      // set questions property value equal to empty array
+      questions = [];
+
+      // add new question to top of array of questions
+      questions.unshift(question);
+
+      // set new array of objects to Local Storage
+      // convert array of objects value into a string value
+      localStorage.setItem('questions', JSON.stringify(questions));
+
+    } else {
+      // get questions property value from Local Storage and store inside local variable named questions
+      questions = JSON.parse(localStorage.getItem('questions'));
+
+      // add new question to array of questions
+      questions.unshift(question);
+
+      // set Local Storage questions property to new array of questions that includes new question
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
   }
 
+  // remove question from Local Storage
   removeQuestion(question: Question) {
-    // remove question from "questions" array of objects property
+    // loop through question objects inside questions property
     for (let i = 0; i < this.questions.length; i++) {
-      // check if current question is the same question as the question interation
+      // check if selected question is the same question as the question interation
       if (question == this.questions[i]) {
-        // spice out that specific question from "questions" property array of objects
+        // remove question from questions property
         this.questions.splice(i, 1);
       }
     }
